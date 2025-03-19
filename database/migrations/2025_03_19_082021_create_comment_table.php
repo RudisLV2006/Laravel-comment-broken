6ed5d464e18f805c34fb2556e580c067192c8338
Vigo28 <gamingvigo06@gmail.com>
2025-03-19 10:40:39 +0200

    Add timestamp; make controller and route

--- database/migrations/2025_03_19_082021_create_comment_table.php
+++ database/migrations/2025_03_19_082021_create_comment_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string("content");
             $table->foreignId("user_id")->constrained()->cascadeOnDelete();
             $table->foreignId("post_id")->constrained()->cascadeOnDelete();
+            $table->timestamps();
         });
     }
 
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('posts', PostController::class);  //1
+
+Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
