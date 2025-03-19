345a419175a3423b7f068fdbd222fc4da15895bd
Vigo28 <gamingvigo06@gmail.com>
2025-03-19 09:49:31 +0200

    Define variable token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -36,7 +37,7 @@ class AuthController extends Controller
             return ['message' => 'The provided creadentials are incorrect.'];
         }
 
-        $user->createToken($user->name);
+        $token = $user->createToken($user->name); //3
         
         return [
             'user' => $user,
