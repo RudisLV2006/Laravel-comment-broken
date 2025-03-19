8f2d4124a7df150f94ffc7f6e8b576c088b2018a
Vigo28 <gamingvigo06@gmail.com>
2025-03-19 10:11:24 +0200

    Policy change method name to modify

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,6 +12,6 @@ class Post extends Model
     protected $fillable = ['title', 'body'];
 
     public function user() {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class); //5
     }
 }
